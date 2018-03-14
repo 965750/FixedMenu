6e2b0bfc95b5b5d20d94dec2393f8298b11efe20
965750 <aventhelast@gmail.com>
2018-03-14 09:19:25 +0100

    style added

--- src/js/main.js
+++ src/js/main.js
@@ -1,33 +1,65 @@
-$(document).ready(function(){
-    
+$(document).ready(function () {
+
     $(".barContRight").hide();
     //hover
-    $(".barContLeft").on("mouseenter", function(){
+    $(".barContLeft").on("mouseenter", function () {
         $(this).next().fadeToggle(200);
     });
-    $(".barContLeft").on("mouseleave", function(){
+    $(".barContLeft").on("mouseleave", function () {
         $(this).next().fadeToggle(150);
     });
     var panel = 0;
     //click
-    $(".barContLeft").on("click", function(){
-        
+    $(".barContLeft").on("click", function () {
+
         $(this).prev().toggleClass("leftPanelActive");
         $(".barContAll").toggleClass("barContAllActive");
-        $(".barContLeft").not(this).fadeToggle(300);
-        
-        if(panel == 0){
+        $(".barContLeft").not(this).fadeToggle(100);
+
+        //panel ON
+        if (panel == 0) {
             $(this).append("<i class='fas fa-times iconFA ys'></i>");
             $(this).find(".nc").fadeOut(0);
-            
+            $(".barContRight").css({
+                width: "0px"
+            });
+            $(this).css({
+                borderTopRightRadius: "10px",
+                borderBottomRightRadius: "10px"
+            });
+
+            var filterVal = 'blur(1px)';
+            $("body > *:not('.barContAll')")
+                .css('filter', filterVal)
+                .css('webkitFilter', filterVal)
+                .css('mozFilter', filterVal)
+                .css('oFilter', filterVal)
+                .css('msFilter', filterVal);
+
             panel = 1;
-            
+
+            //panel OFF    
         } else {
             $(this).find(".ys").hide(0);
             $(this).find(".nc").fadeIn(0);
+            $(".barContRight").css({
+                width: "100px"
+            });
+            $(this).css({
+                borderTopRightRadius: "0px",
+                borderBottomRightRadius: "0px"
+            });
             
-            panel = 0;
+            var filterValClear = 'blur(0px)';
+            $("body > *:not('.barContAll')")
+                .css('filter', filterValClear)
+                .css('webkitFilter', filterValClear)
+                .css('mozFilter', filterValClear)
+                .css('oFilter', filterValClear)
+                .css('msFilter', filterValClear);
 
+            panel = 0;
         }
     });
+
 });
